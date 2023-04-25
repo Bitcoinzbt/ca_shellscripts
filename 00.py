4485dc819df367b37cdfb39392dd3a92462670a4
Christophe C <115646436+Bitcoinzbt@users.noreply.github.com>
2023-04-25 13:36:06 +0200

    Add files via upload

---
+++ 00.py
@@ -0,0 +1,16 @@
+# Creer un programme qui affiche l'alphabet en lettre minuscules suivi d'une retour a la ligne.
+
+#=========================== l'alphabet ========================# 00.py
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+for letter in alphabet:
+    print(letter, end="")
+print("\n")
+
+
+          
+
+
+          
+
+
